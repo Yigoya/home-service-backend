889c7acea2a4a091349d5f66dec4d4f45fc625f0
yigermal abebe <yigoya7@gmail.com>
2025-01-31 21:33:03 +0300

    fix admin service

--- src/main/java/com/home/service/controller/ServiceManagementController.java
+++ src/main/java/com/home/service/controller/ServiceManagementController.java
@@ -79,7 +79,7 @@ public class ServiceManagementController {
 
     @GetMapping("/test")
     public ResponseEntity<String> testEndpoint() {
-        return ResponseEntity.ok("Test endpoint is working! 🎉 1");
+        return ResponseEntity.ok("Test endpoint is working! 🎉 2");
     }
 
     // Technician Endpoints
